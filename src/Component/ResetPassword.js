58eea7be44893d407c644a7309f65569a717f6cd
jahidul <programmerjahid162@gmail.com>
2022-08-19 15:36:55 +0600

    adding reset Password

--- src/Component/ResetPassword.js
+++ src/Component/ResetPassword.js
@@ -1,130 +1,220 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import LoginTopHeader from "../Sheard/LoginTopHeader";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import './ResetPassword.css';
+import "./ResetPassword.css";
 import { Base64 } from "js-base64";
 
 const ResetPassword = () => {
   const ConfirmResetPasswordPage = useNavigate();
-  const [password,setPassword] = useState();
-  const [confirmpassword,setConfirmPassword] = useState();
+  const [password, setPassword] = useState();
+  const [confirmpassword, setConfirmPassword] = useState();
   const [expire, setExpire] = useState();
-  const [errormsg,setErrorMsg] = useState();
-    const {id,token} = useParams();
+  const [errormsg, setErrorMsg] = useState();
+  const { id, token } = useParams();
 
-    const ResetPassword = async() => {
-      try{
-        const {data} = await axios.post("https://investmentportal.herokuapp.com/verifyForgotMail",{token})
-        console.log(data?.result);
-        if(data?.result === "Link expired"){
-          setExpire(true);
-        }else{
-          setExpire(false);
-          if(!password || !confirmpassword){
-            setErrorMsg("Plz fill up all fields!");
-          }else if (password !== confirmpassword) {
-            setErrorMsg("password not matched!");
-          }else if (password.length < 8) {
-            setErrorMsg("Password is too short!");
-          }else{
-              axios.put("https://investmentportal.herokuapp.com/reset-password",{id,password}).then(function(data){
+  const ResetPassword = async () => {
+    try {
+      const { data } = await axios.post(
+        "https://investmentportal.herokuapp.com/verifyForgotMail",
+        { token }
+      );
+      console.log(data?.result);
+      if (data?.result === "Link expired") {
+        setExpire(true);
+      } else {
+        setExpire(false);
+        if (!password || !confirmpassword) {
+          setErrorMsg("Please fill up all fields!");
+        } else if (password !== confirmpassword) {
+          setErrorMsg("Password not matched!");
+        } else if (password.length < 8) {
+          setErrorMsg("Password is too short!");
+        } else {
+          axios
+            .put("https://investmentportal.herokuapp.com/reset-password", {
+              id,
+              password,
+            })
+            .then(function (data) {
               //console.log(data);
-              if(data.data.message === "Data Updated Successfully"){
+              if (data.data.message === "Data Updated Successfully") {
                 ConfirmResetPasswordPage("/confirmresetpassword");
-              }else{
-                setErrorMsg("something wrong! try again later");
+              } else {
+                setErrorMsg("Something wrong! try again later");
               }
-              })
-          }
+            });
         }
-      }catch(error){
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(id);
+    console.log(token);
+    const CheckValidLink = async () => {
+      try {
+        const { data } = await axios.post(
+          "https://investmentportal.herokuapp.com/verifyForgotMail",
+          { token }
+        );
+        console.log(data?.result);
+        if (data?.result === "Link expired") {
+          setExpire(true);
+        } else {
+          setExpire(false);
+        }
+      } catch (error) {
         console.log(error);
       }
-      
-    }
+    };
+    CheckValidLink();
+  }, [token]);
 
-    useEffect(()=>{
-      console.log(id);
-      console.log(token);
-      const CheckValidLink = async()=>{
-        try{
-          const {data} = await axios.post("https://investmentportal.herokuapp.com/verifyForgotMail",{token})
-          console.log(data?.result);
-          if(data?.result === "Link expired"){
-            setExpire(true);
-          }else{
-            setExpire(false);
-          }
-        }catch(error){
-          console.log(error);
-        }
-      }
-      CheckValidLink();
-    },[token]);
+  return (
+    <div>
+      <LoginTopHeader></LoginTopHeader>
 
-    return(
-        <div className="container">
-            <LoginTopHeader></LoginTopHeader>
-            <div className="form-content">
-              {expire === false &&(
-                <>
-                <p style={{color:"white",backgroundColor:"blue"}}>Your link will expire in 5 minutes.</p>
-                <div className="form-group mt-1" style={{width:"50%"}}>
-                <input
-                  style={{
-                    backgroundColor: "#00ADEE",
-                    color: "white",
-                  }}
-                  onChange={(e)=>setPassword(e.target.value)}
-                  type="password"
-                  className="form-control"
-                  id=""
-                  aria-describedby="username"
-                  placeholder="New Password"
-                  required
-                  // onChange={(e)=>setResetEmail(e.target.value)}
-                />
-               </div>
-              <div class="form-group mt-1" style={{width:"50%"}}>
-                            <input
-                              style={{
-                                backgroundColor: "#00ADEE",
-                                color: "white",
-                              }}
-                              onChange={(e)=>setConfirmPassword(e.target.value)}
-                              type="password"
-                              className="form-control"
-                              id=""
-                              aria-describedby="username"
-                              placeholder="Confirm New Password"
-                              required
-                              // onChange={(e)=>setResetEmail(e.target.value)}
-                            />
-              </div>
-              <div style={{alignItems:"center",marginTop:"10px"}}>
-              <button className="resetbtn" onClick={ResetPassword}>Reset Password</button>
-              </div>
-              <div>
-                <p style={{color:"red"}}>{errormsg}</p>
-              </div>
-              </>
-              )}
+      <div className="form-content">
+        {expire === false && (
+          <div class="container">
+            <div class="row justify-content-md-center">
+              <div class="col-md-6 text-center">
+                <div class="row">
+                  <div className="col-sm-12 mt-5 ">
+                    <div className="card text-white bg-dark ">
+                      <div className="card-header text-center p-3 text-warning">
+                        Your link will expire in 5 minutes.
+                      </div>
 
-              {expire === true &&(
-                <>
-                <div className="form-group mt-1" style={{width:"50%"}}>
-                <h1 className="expiredlink">Your link has expired, please try to reset your password using the Forgot Password link.</h1>
+                      <div class="card-body mt-2">
+                        <div
+                          className="form-group mt-1"
+                          style={{ width: "50%" }}
+                        >
+                          <input
+                            style={{
+                              backgroundColor: "#00ADEE",
+                              color: "white",
+                            }}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                            className="form-control"
+                            id=""
+                            aria-describedby="username"
+                            placeholder="New Password"
+                            required
+                            // onChange={(e)=>setResetEmail(e.target.value)}
+                          />
+                        </div>
+                        <div class="form-group mt-1" style={{ width: "50%" }}>
+                          <input
+                            style={{
+                              backgroundColor: "#00ADEE",
+                              color: "white",
+                            }}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            type="password"
+                            className="form-control"
+                            id=""
+                            aria-describedby="username"
+                            placeholder="Confirm New Password"
+                            required
+                            // onChange={(e)=>setResetEmail(e.target.value)}
+                          />
+                        </div>
+
+                        <div class="form-group mt-3">
+                          <button
+                            //  style={{ float: "left", color: "#00ADEE" }}
+                            type="button"
+                            className="btn btn-primary mt-2 border-0"
+                            onClick={ResetPassword}
+                            id="registetionbutton"
+                          >
+                            Sign In
+                          </button>
+                          {/* <p style={{ color: "red" }}>{alertmsg}</p> */}
+
+                          <div class="alert alert-danger" role="alert">
+                            {errormsg}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              </>
-              )}
-            
-            
-            
             </div>
-            
-        </div>
-    )
-}
+          </div>
+        )}
+        {expire === true && (
+          <>
+            <div class="alert alert-danger" role="alert">
+              Your link has expired, please try to reset your password using the
+              Forgot Password link.
+            </div>
+          </>
+        )}
+        {/* <p style={{ color: "white", backgroundColor: "blue" }}>
+              Your link will expire in 5 minutes.
+            </p>
+            <div className="form-group mt-1" style={{ width: "50%" }}>
+              <input
+                style={{
+                  backgroundColor: "#00ADEE",
+                  color: "white",
+                }}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                className="form-control"
+                id=""
+                aria-describedby="username"
+                placeholder="New Password"
+                required
+                // onChange={(e)=>setResetEmail(e.target.value)}
+              />
+            </div>
+            <div class="form-group mt-1" style={{ width: "50%" }}>
+              <input
+                style={{
+                  backgroundColor: "#00ADEE",
+                  color: "white",
+                }}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type="password"
+                className="form-control"
+                id=""
+                aria-describedby="username"
+                placeholder="Confirm New Password"
+                required
+                // onChange={(e)=>setResetEmail(e.target.value)}
+              />
+            </div>
+            <div style={{ alignItems: "center", marginTop: "10px" }}>
+              <button className="resetbtn" onClick={ResetPassword}>
+                Reset Password
+              </button>
+            </div>
+            <div>
+              <p style={{ color: "red" }}>{errormsg}</p>
+            </div>
+          </>
+        )}
+
+        {expire === true && (
+          <>
+            <div className="form-group mt-1" style={{ width: "50%" }}>
+              <h1 className="expiredlink">
+                Your link has expired, please try to reset your password using
+                the Forgot Password link.
+              </h1>
+            </div> */}
+      </div>
+    </div>
+  );
+};
 
 export default ResetPassword;
